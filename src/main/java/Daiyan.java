84747cd67261eb8902dd267c6f9fca5ca3eb8494
killmenowya <handthad@gmail.com>
2025-01-23 18:18:44 +0800

    Completed the task for level 0

---
+++ src/main/java/Daiyan.java
@@ -0,0 +1,9 @@
+public class Daiyan {
+  public static void main(String[] args) {
+    String asciiArt = " ____   ____   _ __  __ ____   __  _ \n" +
+                      "| _) \\ / () \\ | |\\ \\/ // () \\ |  \\| |\n" +
+                      "|____//__/\\__\\|_| |__|/__\\/__\\|_|\\__|"; 
+    System.out.println(asciiArt);
+    System.out.println("__________________________________________\nHello I'm Daiyan\nWhat can I do for you?\n__________________________________________\nBye. Hope to see you again soon!");
+  }
+}
